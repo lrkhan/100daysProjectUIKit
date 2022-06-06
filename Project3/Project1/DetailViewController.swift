8511f1ba4dfd7f938aea49de21390e6ecefc80e3
LuthforKhan <lrkhan@umich.edu>
2022-06-06 09:29:09 -0400

    completed project 3

---
+++ Project3/Project1/DetailViewController.swift
@@ -0,0 +1,64 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Luthfor Khan on 6/5/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    var count: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = count
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let img = selectedImage {
+            imageView.image = UIImage(named: img)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    @objc func shareTapped() {
+        guard let img = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [img], applicationActivities: [])
+        
+        // needed for an iPad
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(vc, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
